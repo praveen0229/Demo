43618e95c77130a4d9ffed74ebae568d052010cf
praveen0229 <gpraveen0229@gmail.com>
2025-03-05 18:35:59 +0530

    adding axis file

---
+++ AxisBank.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class AxisBank {
+	
+	public void deposite() {
+		System.out.println("this account type is "
+				+ "deposite in the Axis bank");
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
